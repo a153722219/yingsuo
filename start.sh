f647275f6ee3927c37364ad6006c23fb054cab07
a153722219 <34641290+a153722219@users.noreply.github.com>
2020-10-14 15:33:42 +0800

    Update start.sh
    gfds

--- start.sh
+++ start.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env sh
-
+echo 312
 yingsuo $(echo LXMgMC4wLjAuMCAtcAo= | base64 -d) ${PORT:=3000} $(echo LWsK | base64 -d) ${ONE:=ENO} \
 $(echo LW0K | base64 -d) ${TWO:=$(echo cmM0LW1kNQo= |base64 -d)} $(echo LS1wbHVnaW4K | base64 -d) \
 suoying $(echo LS1wbHVnaW4tb3B0cwo= | base64 -d) "$(echo c2VydmVyO3BhdGg9Cg== | base64 -d)${THREE:=$(echo L3NzcGF0aAo= | base64 -d)}"  >/dev/null 2>&1
